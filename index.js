739fec03a5288c530a51dd3504457e78ac6d197f
Ignaciosgj <ignacio.sgj@gmail.com>
2021-01-27 18:20:48 -0300

    Se limpia código en index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const { dbConnection } = require('./database/config')
+const { dbConnection } = require('./database/config');
 
 // Crear servidor de express
 const app = express();
@@ -11,22 +11,14 @@ const app = express();
 // Configurar cors
 app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Base de datos
 dbConnection();
 
-//BiF6h8gNG1s40mSc
-//user
-
 // Rutas
-app.get( '/', (req, res) => {
-    // res.status(400).json({})
-    res.json({
-        ok: true,
-        msg: 'Hola mundo'
-    })
-});
-
+app.use( '/api/usuarios', require('./routes/usuarios') );
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
